f541a950bcba524cca129489110f9cc0fe7e7660
Steff Stange <stefaniastange@gmail.com>
2021-08-16 13:36:17 -0300

    Index creation
    Made my first try with variables and echoes.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?PHP 
+$name = "steffi"; 
+$num1 = 5;
+$num2 = 2;
+$suma = $num1 + $num2;
+echo "hola, soy ". $name. "\n"; 
+echo "la suma de " . $num1 . " más " . $num2 . " es: " . $suma; 
+
+?>
+
